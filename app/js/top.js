b7f7aed3711968b79f5e05e968aa19253f48c7b4
izumin5210 <masayuki@izumin.info>
2014-10-25 11:01:48 +0900

    目的地設定機能（近づくと激しく明滅）

--- app/js/top.js
+++ app/js/top.js
@@ -2,6 +2,13 @@ $(function() {
   // k.showDebugLog();
   var $containerUnconnected = $('#container-unconnected');
   var $containerConnected = $('#container-connected');
+  // $containerConnected.hide();
+
+  var map, latitude, longitude, myLatLng;
+  var dest, destLat, destLng, distance;
+  var blinkTimer, currentLedState = 0;
+
+  // グラフ
   var graphX = new Graph('#graph-acc-x', {
     max: { y: 1300 },
     color: '#ff9999'
@@ -15,8 +22,73 @@ $(function() {
     color: '#9999ff'
   });
 
-  $containerConnected.hide();
+  function calcDistance() {
+    destLat = dest.geometry.location.lat();
+    destLng = dest.geometry.location.lng();
+    distance = geolib.getDistance(
+        { latitude: latitude, longitude: longitude },
+        { latitude: destLat, longitude: destLng }
+        );
+    $('#distance').text(distance);
 
+    if (blinkTimer != null) {
+      clearInterval(blinkTimer);
+      blinkTimer = null;
+    }
+    if (distance < 50) {
+      blinkTimer = setInterval(function() {
+        currentLedState = (currentLedState === 0) ? 100 : 0;
+        k.pwmLedDrive(k.PIO1, currentLedState);
+      }, 1000);
+    } else if (distance < 100) {
+      blinkTimer = setInterval(function() {
+        currentLedState = (currentLedState === 0) ? 80 : 0;
+        k.pwmLedDrive(k.PIO1, currentLedState);
+      }, 2000);
+    } else if (distance < 200) {
+      blinkTimer = setInterval(function() {
+        currentLedState = (currentLedState === 0) ? 70 : 0;
+        k.pwmLedDrive(k.PIO1, currentLedState);
+      }, 3000);
+    } else if (distance < 500) {
+      blinkTimer = setInterval(function() {
+        currentLedState = (currentLedState === 0) ? 70 : 0;
+        k.pwmLedDrive(k.PIO1, currentLedState);
+      }, 5000);
+    }
+  }
+
+  // 位置情報の監視
+  navigator.geolocation.watchPosition(function(pos) {
+    latitude = pos.coords.latitude;
+    longitude = pos.coords.longitude;
+    myLatLng = new google.maps.LatLng(latitude, longitude);
+    $('#latitude').text(latitude);
+    $('#longitude').text(longitude);
+    if (map == null) initializeMap();
+    if (dest != null) calcDistance();
+  });
+
+  var initializeMap = function() {
+    var mapOpts = {
+      center: myLatLng, zoom: 11, mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map_canvas'), mapOpts);
+  };
+
+  // 目的地の設定
+  $('#form-dest').on('submit', function() {
+    var destName = $('#form-dest input[name=dest]').val();
+    var service = new google.maps.places.PlacesService(map);
+    service.textSearch({ query: destName }, function(result, status) {
+      dest = result[0];
+      $('#dest').text(result[0].name);
+      calcDistance();
+    });
+    return false;
+  });
+
+  // konashiのコールバックたち
   var onBtnConnectClicked = function(e) {
     k.find();
   };
@@ -35,14 +107,13 @@ $(function() {
     var accPins = {};
     // var currentLedState = k.LOW;
     // k.pinMode(k.PIO1, k.OUTPUT);
-    var currentLedState = 0;
     k.pwmMode(k.PIO1, k.KONASHI_PWM_ENABLE_LED_MODE);
-    setInterval(function() {
-      // currentLedState = (currentLedState === k.HIGH) ? k.LOW : k.HIGH;
-      // k.digitalWrite(k.PIO1, currentLedState);
-      currentLedState = (currentLedState < 100) ? (currentLedState+1): 0;
-      k.pwmLedDrive(k.PIO1, currentLedState);
-    }, 10);
+    // setInterval(function() {
+      // // currentLedState = (currentLedState === k.HIGH) ? k.LOW : k.HIGH;
+      // // k.digitalWrite(k.PIO1, currentLedState);
+      // currentLedState = (currentLedState < 100) ? (currentLedState+1): 0;
+      // k.pwmLedDrive(k.PIO1, currentLedState);
+    // }, 10);
     setInterval(function() {
       $.each([k.AIO0, k.AIO1, k.AIO2], function(pin) {
         k.analogReadRequest(pin);
